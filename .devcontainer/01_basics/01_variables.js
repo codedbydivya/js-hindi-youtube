658377cdae599b67dab27428cfbdb20c92ee4c24
codedbydivya <divyajaiswa05@gmail.com>
2025-06-16 13:24:02 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 122553
+let accountEmail ="divyajaiswal@gmail.com"
+var accountPassword = "1234"
+accountCity = "Mumbai"
+ 
+// accountId = 2 // not allowed
+
+console.log(accountId);
